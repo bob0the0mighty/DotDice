8674ae988951a2763da85b8e6255b527596907ed
Dave <bob0the0mighty@gmail.com>
2025-02-20 13:42:43 -0500

    removed f# proj

--- DotDiceF/AST/Roll.fs
+++
@@ -1,9 +0,0 @@
-namespace DotDiceF.Ast
-
-open DotDiceF.Ast
-
-/// Represents a node in the abstract syntax tree (AST) for a dice roll expression.
-type Roll =
-    | BasicRoll of numberOfDice:int * dieType:int * isFudge:bool * isPercentile: bool * modifiers:Modifier list
-    | GroupedRoll of rolls:Roll list * modifiers:Modifier list
-    | Constant of value:int
--- DotDiceF/Core/ParserResult.fs
+++
@@ -1,6 +0,0 @@
-namespace DotDiceF.Core
-
-/// Represents the result of a parsing operation.
-type ParserResult<'T> =
-    | Success of 'T * string  // Success: Contains the parsed value and the remaining input string.
-    | Failure of string      // Failure: Contains an error message.
